92ad6708421a8c544c814d5751a05c5837d9729c
SupRobin <flavio.m.duran@gmail.com>
2025-03-12 16:29:50 -0400

    Adding stuff

--- main.cpp
+++ main.cpp
@@ -5,44 +5,47 @@
 const unsigned int BYTES = 32;
 
 struct Employee {
-    std::string name;
-    long salary;
+    char* name;
+    int salary = 0;
 };
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
 
-    int fd = open("./Employee.txt", O_RDONLY);
-    if (fd == -1) {
-        perror("open");
+char* fksHash(Employee name) {
+    static char hash[BYTES];
+
+    return hash;
+}
+
+void openReadWriteAndClose(const char *fileName, char *apples){
+    int fd = open(fileName, O_RDWR);
+    int bf = open("data.bin", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1 || bf == -1) {
+        perror("open/create");
         exit(1);
     }
 
-    const char *apples[BYTES];
-    while (read(fd, apples, BYTES)) {
-        for (auto & apple : apples) {
-            std::cout << apple;
+    std::cout << "Opened file " << fd << std::endl;
+    std::cout << "Created file " << bf << std::endl;
+    ssize_t bytesRead;
+    ssize_t bytesWritten;
+    while ((bytesRead  = read(fd, apples, BYTES)) > 0) {
+        apples[bytesRead] = '\0';
+        bytesWritten = write(bf, apples, BYTES);
+        if (bytesWritten != sizeof(Employee)) {
+            perror("write");
         }
     }
-    ssize_t bytes_read = read(fd, apples, BYTES);
-    if (bytes_read == -1) {
-        perror("read");
-        exit(2);
-    }
 
-    std::cout << apples << std::endl;
-    std::cout << apples[0] << std::endl;
-    std::cout << "hello" << std::endl;
+    close(fd);
+}
 
-    if (lseek(fd, 0, SEEK_SET) == -1) {
-        perror("lseek");
-        exit(3);
-    }
+int main(const int argc, const char *argv[]) {
+    char *apples;
+    const char *fileName = argv[1];
+    std::cout << argc << std::endl;
+    std::cout << fileName << std::endl;
+
+    openReadWriteAndClose(fileName, apples);
 
     return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
